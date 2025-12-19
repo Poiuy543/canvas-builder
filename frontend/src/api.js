3f1f8be63f929cfcc54365363597d22e3267df2b
Arjun Barman <arjunbarman5555@gmail.com>
2025-12-19 14:35:57 +0530

    Point frontend to Render backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://canvas-builder-backend.onrender.com/api/canvas'
+  baseURL: 'https://canvas-builder-backend-onvw.onrender.com/api/canvas'
 });
 
 export default api;
